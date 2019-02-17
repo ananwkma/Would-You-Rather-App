410f891c7191ed956722ad8a5d440909e9fb016c
Anan <anan.w.ma@gmail.com>
2019-02-17 15:09:19 -0800

    Updates

---
+++ precache-manifest.9ad049509798d4f5480c7736318c6282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa7e287f7e0026eebe4a",
+    "url": "/Would-You-Rather-App/static/js/runtime~main.aa7e287f.js"
+  },
+  {
+    "revision": "216e7bd2274daea2f75a",
+    "url": "/Would-You-Rather-App/static/js/main.216e7bd2.chunk.js"
+  },
+  {
+    "revision": "8659f32648911d734653",
+    "url": "/Would-You-Rather-App/static/js/1.8659f326.chunk.js"
+  },
+  {
+    "revision": "216e7bd2274daea2f75a",
+    "url": "/Would-You-Rather-App/static/css/main.dff3f24e.chunk.css"
+  },
+  {
+    "revision": "0ed97c5e4d31a11bea8b7c742a11c6a2",
+    "url": "/Would-You-Rather-App/index.html"
+  }
+];
